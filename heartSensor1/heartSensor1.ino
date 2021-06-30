a661d09c760a1e120c402fde83dd1fb51bd74c40
amos <amos.aulbach@gmx.de>
2021-06-30 13:54:59 +0200

    added time stamps
    timestamps added, data is collected in an array. Artuino doesn't send data as a json anymore

--- heartSensor1/heartSensor1.ino
+++ heartSensor1/heartSensor1.ino
@@ -56,26 +56,26 @@ void setup(){
 
   Wire.begin();
   int result = bioHub.begin();
-  if (result == 0) // Zero errors!
-    Serial.println("Sensor started!");
-  else
-    Serial.println("Could not communicate with the sensor!!!");
+  //if (result == 0) // Zero errors!
+    //Serial.println("Sensor started!");
+  //else
+    //Serial.println("Could not communicate with the sensor!!!");
  
-  Serial.println("Configuring Sensor...."); 
+  //Serial.println("Configuring Sensor...."); 
   int error = bioHub.configBpm(MODE_ONE); // Configuring just the BPM settings. 
   if(error == 0){ // Zero errors!
-    Serial.println("Sensor configured.");
+    //Serial.println("Sensor configured.");
   }
   else {
-    Serial.println("Error configuring sensor.");
-    Serial.print("Error: "); 
-    Serial.println(error); 
+    //Serial.println("Error configuring sensor.");
+    //Serial.print("Error: "); 
+    //Serial.println(error); 
   }
 
   // Data lags a bit behind the sensor, if you're finger is on the sensor when
   // it's being configured this delay will give some time for the data to catch
   // up. 
-  Serial.println("Loading up the buffer with data....");
+  //Serial.println("Loading up the buffer with data....");
   delay(4000); 
 
 
@@ -103,11 +103,16 @@ void loop(){
     delay(250);
     //char JSONMessage[] = " {\"Value\": body.heartRate}";
     int heartRate = body.heartRate;
+
+    /*
     DynamicJsonBuffer heartBuffer;
     JsonObject& root = heartBuffer.createObject();
     root["Herzschlag"] = heartRate;
     root.prettyPrintTo(Serial);
     Serial.println();
+    */
+
+    Serial.println(heartRate);
     
 
 
--- serialport.js
+++ serialport.js
@@ -4,8 +4,8 @@ var readline = require('@serialport/parser-readline');
 var serial = SerialPort.serialPort;
 var com = new SerialPort("com3",{baudRate:115200});
 
-
-
+var Results = []
+var date;
 com.on("open",open);
 
 function open(){
@@ -18,5 +18,9 @@ var parser = com.pipe(new readline({delimiter:'\r\n'}));
 parser.on("data",getData);
 
 function getData(data){
-  console.log(data);
+  date= new Date ();
+  //console.log(data, date);
+  Results.push({Herzschlag: data, Uhrzeit: date});
+  console.log(Results);
+
 }
