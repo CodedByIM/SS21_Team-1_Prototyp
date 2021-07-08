bcc8c87db0d1c215a2a5bad0e6e84e759d0b0718
amos <amos.aulbach@gmx.de>
2021-07-08 10:12:22 +0200

    deleted unnecessary stuff

--- check.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-var results = { table: [] };
-fs.readFile('data2.txt', 'utf8', (err, data)=>{
-    results.table.push(JSON.parse(data));
-    console.log(results, results.table.length);
-    if(err) return console.log(err);
-  })
